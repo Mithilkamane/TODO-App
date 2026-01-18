58c5f46bd868b9f0032568c5e214e58ff16b881a
mithil <mithilkamane2006@gmail.com>
2026-01-19 00:03:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const input = document.getElementById("todoInput");
+const addBtn = document.getElementById("addBtn");
+const todoList = document.getElementById("todoList");
+
+addBtn.addEventListener("click", addTodo);
+
+function addTodo() {
+  if (input.value.trim() === "") {
+    alert("Please enter a task");
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.innerText = input.value;
+
+  // mark complete
+  li.addEventListener("click", () => {
+    li.classList.toggle("completed");
+  });
+
+  // delete button
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "X";
+  delBtn.addEventListener("click", () => {
+    li.remove();
+  });
+
+  li.appendChild(delBtn);
+  todoList.appendChild(li);
+
+  input.value = "";
+}
